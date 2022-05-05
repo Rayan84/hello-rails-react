c2861e41ec88776843b6225c8f0dee11cb35940b
Rayan84 <rayan_84@live.com>
2022-05-05 21:25:07 +0200

    Add redux dependencies Redux basic file structure Redux store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Greetings from './Greetings';
+import configureStore from '../redux/configureStore';
+import { Provider } from 'react-redux';
+
+const store = configureStore();
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Greetings />} />
-          </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Greetings />} />
+            </Routes>
+          </BrowserRouter>
+        </Provider>
       </header>
     </div>
   );
---
+++ app/javascript/redux/configureStore.js
@@ -0,0 +1,23 @@
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+const initialState = {
+  greetings: []
+};
+
+function rootReducer(state, action) {
+  console.log(action.type);
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+export default function configureStore() {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk)
+  );
+  return store;
+}
